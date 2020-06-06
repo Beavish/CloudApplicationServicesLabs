56bbb4237a09999eb07d315fc7c926b16e4e0fd1
Beavish <kieran.philip.williams@gmail.com>
2020-06-06 17:00:22 +0100

    first lab

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  get 'info/infogem'
+
+  get 'info/infoauthor'
+
+  get 'info/infolatestgems'
+
+  get 'welcome/index'
+
+  root "welcome#index"
+
+  match 'searchgem', to: 'info#infogem', via: [:get, :post]
+
+
+  match 'searchauthor', to: 'info#infoauthor', via: [:get, :post]
+
+  resources :dependencies
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
